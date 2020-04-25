6d89e105ffed4a2bd26add6645a1a44b2219fc20
Kliver Daniel Giron Castro <kliver.giron1@correo.icesi.edu.co>
2020-04-25 00:44:03 -0500

    Se agrego implementacion que permite al usuario detener el juego presionando la tecla <space> tambien con click derecho sobre una celula permite dibujar en esta y con clic izquierdo eliminar la celula dibujada.

--- juego_de_la_vida.py
+++ juego_de_la_vida.py
@@ -4,15 +4,18 @@ import time
 
 pg.init()
 
-width, height = 700, 700
+width, height = 800, 800
 screen = pg.display.set_mode((height, width))
+clock = pg.time.Clock()
+fps = 24 # mas pequeño = mas lento el programa en detectar mouse y clicks
 
 bg = 25, 25, 25
 c_point = [(128, 128, 128),(255, 255, 255)]
 screen.fill(bg)
 run = True
+pauseExect = False
 
-nxC, nyC = 25, 25
+nxC, nyC = 50, 50
 
 dimCW = width / nxC
 dimCH = height / nyC
@@ -20,17 +23,13 @@ dimCH = height / nyC
 # Estructura de datos, almacena los estados. 1 = viva, 0 = muerta
 gameState = np.zeros((nxC, nyC))
 
-#Pruebas, iniciando estados
-#gameState[5, 3] = 1
-#gameState[5, 4] = 1
-#gameState[5, 5] = 1
-
-gameState[21, 21] = 1
-gameState[22, 22] = 1
-gameState[22, 23] = 1
-gameState[21, 23] = 1
-gameState[20, 23] = 1
-#Fin pruebas
+#Estado inicial
+gameState[1, 1] = 1
+gameState[2, 2] = 1
+gameState[2, 3] = 1
+gameState[1, 3] = 1
+gameState[0, 3] = 1
+#Fin estado inicial
 
 while run:
 
@@ -38,28 +37,43 @@ while run:
 	newGameState = np.copy(gameState)
 
 	screen.fill(bg)
-	time.sleep(0.05)
+	time.sleep(0.01)
+	clock.tick(fps)
+
+	for event in pg.event.get(): # User did something
+		if event.type == pg.QUIT:
+			run = False
+		elif event.type == pg.KEYDOWN:
+			pauseExect = not pauseExect
+
+		mouseClick = pg.mouse.get_pressed()
+		if sum(mouseClick) > 0:
+			posX, posY = pg.mouse.get_pos()
+			celX, celY = int(np.floor(posX / dimCW)), int(np.floor(posY / dimCH))
+			newGameState[celX][celY] = not mouseClick[2]
 
 	for y in range(0, nxC):
 		for x in range(0, nyC):
 
-			#Vecinos cercanos
-			n_neigh = 	gameState[(x-1) % nxC, (y-1) % nyC] + \
-						gameState[x 	% nxC, (y-1) % nyC] + \
-						gameState[(x+1) % nxC, (y-1) % nyC] + \
-						gameState[(x-1) % nxC, y 	 % nyC] + \
-						gameState[(x+1) % nxC, y 	 % nyC] + \
-						gameState[(x-1) % nxC, (y+1) % nyC] + \
-						gameState[x 	% nxC, (y+1) % nyC] + \
-						gameState[(x+1) % nxC, (y+1) % nyC]
+			if not pauseExect:
 
-			#Regla 1: revive
-			if gameState[x,y] == 0 and (n_neigh == 3):
-				newGameState[x,y] = 1
+				#Vecinos cercanos
+				n_neigh = 	gameState[(x-1) % nxC, (y-1) % nyC] + \
+							gameState[x 	% nxC, (y-1) % nyC] + \
+							gameState[(x+1) % nxC, (y-1) % nyC] + \
+							gameState[(x-1) % nxC, y 	 % nyC] + \
+							gameState[(x+1) % nxC, y 	 % nyC] + \
+							gameState[(x-1) % nxC, (y+1) % nyC] + \
+							gameState[x 	% nxC, (y+1) % nyC] + \
+							gameState[(x+1) % nxC, (y+1) % nyC]
 
-			#Regla 2: muere
-			elif gameState[x,y] == 1 and (n_neigh <2 or n_neigh>3):
-				newGameState[x,y] = 0
+				#Regla 1: revive
+				if gameState[x,y] == 0 and (n_neigh == 3):
+					newGameState[x,y] = 1
+
+				#Regla 2: muere
+				elif gameState[x,y] == 1 and (n_neigh <2 or n_neigh>3):
+					newGameState[x,y] = 0
 
 			poly = [((x) * dimCW, y * dimCH),
 					((x+1) * dimCW, y * dimCH),
@@ -72,9 +86,6 @@ while run:
 				pg.draw.polygon(screen, c_point[1], poly, 0)
 
 	gameState = np.copy(newGameState)
-	for event in pg.event.get(): # User did something
-		if event.type == pg.QUIT:
-			run = False
 	pg.display.flip()
 
 pg.quit()
